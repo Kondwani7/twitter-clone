f47e97b4714153318e9a99af74c93465a931c16e
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-09-23 16:26:45 +0200

    twitter feed widget embedded

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {FiSearch} from 'react-icons/fi'
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
+
 function Widgets() {
   return (
     <div className='px-2'>
@@ -12,6 +15,12 @@ function Widgets() {
             className='flex-1 bg-transparent outline-none'
         />
       </div>
+
+       <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="kondwani_g"
+            options={{height: 400}}
+        />
       
     </div>
   )
